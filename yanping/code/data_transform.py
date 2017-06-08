6aad58e51f2dca97ca699168e8813a9f05c9f29d
Yanping <rea.s.xyp@gmail.com>
2017-06-08 05:35:49 +0200

    a

---
+++ yanping/code/data_transform.py
@@ -0,0 +1,41 @@
+def write_to_result(x,y,out1,out2):
+  x = x.replace("...","")
+  x = x.replace("="," ")
+  x = x.replace("\t"," ")
+  y = y.replace("...","")
+  y = y.replace("\t"," ")
+  y = y.replace("="," ")
+
+  out1.write(x+"\n")
+  out2.write(y+"\n")
+
+f = open("../data/new/movie_lines.txt","r")
+line = f.readline()
+mylines = {}
+while line:
+  t = line.strip().split(" +++$+++ ")
+  if (len(t)>4):
+    mylines[t[0]] = t[4]
+  line = f.readline()
+f.close()
+
+f = open("../data/new/movie_conversations.txt","r")
+out1 = open("../data/new/source.txt","w")
+out2 = open("../data/new/target.txt","w")
+line = f.readline()
+while line:
+  t = line.strip().split(" +++$+++ ")
+  con = eval(t[3])
+  p=True
+  for i in range(0,len(con)):
+    if (con[i] not in mylines):
+      p=False
+  if (p):
+    last = mylines[con[0]]
+    for i in range(1,len(con)):
+      write_to_result(last,mylines[con[i]],out1,out2)
+      last = mylines[con[i]]
+  line = f.readline()
+f.close()
+out1.close()
+out2.close()
